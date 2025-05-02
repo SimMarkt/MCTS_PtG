2f93cd8771830e66ef091c310ede15f0bde7f7fd
SimMarkt <s.markth@posteo.de>
2025-05-02 15:15:42 +0200

    update 02052025

--- mcts_main.py
+++ mcts_main.py
@@ -12,11 +12,12 @@ import os
 import torch as th
 
 # Library for the RL environment
-from gymnasium.envs.registration import registry, register 
+from gymnasium.envs.registration import registry, register
+import gymnasium as gym
 
 # Libraries with utility functions and classes
-from src.rl_utils import load_data, initial_print, config_print, Preprocessing, Postprocessing, create_vec_envs#, create_vec_envs
-from src.rl_config_mcts import MCTSConfiguration
+from src.rl_utils import load_data, initial_print, config_print, Preprocessing, Postprocessing
+from src.rl_config_mcts import AgentConfiguration
 from src.rl_config_env import EnvConfiguration
 from src.rl_config_train import TrainConfiguration
 
@@ -26,6 +27,8 @@ from typing import Any, List
 from tqdm import tqdm
 import numpy as np
 import copy
+
+import matplotlib.pyplot as plt
 
 class MCTSNode:
     def __init__(self, env, parent=None, action=None, done=False, remaining_steps=72):
@@ -62,7 +65,7 @@ class MCTS:
         self.iterations = iterations
         self.exploration_weight = exploration_weight
 
-    def search(self, root_env):
+    def search(self, root_env, rootnodeid):
         root_env_copy = copy.deepcopy(root_env)
         root_node = MCTSNode(root_env_copy)
 
@@ -164,7 +167,7 @@ def check_env(env_id):
 def main():
     # --------------------------------------Initialize the RL configuration---------------------------------------
     initial_print()
-    MCTSConfig = MCTSConfiguration()
+    MCTSConfig = AgentConfiguration()
     EnvConfig = EnvConfiguration()
     TrainConfig = TrainConfiguration()
     computational_resources(TrainConfig)
@@ -185,11 +188,13 @@ def main():
     print("Load environment...")
     env_id = 'PtGEnv-v0'
     check_env(env_id)                                                                                                   # Check the Gymnasium environment registry
-    env_train, env_test_post, eval_callback_val, eval_callback_test = create_vec_envs(env_id, str_id, MCTSConfig, TrainConfig, env_kwargs_data)          # Create vectorized environments
+    # env_train, env_test_post, eval_callback_val, eval_callback_test = create_vec_envs(env_id, str_id, MCTSConfig, TrainConfig, env_kwargs_data)          # Create vectorized environments
         
     # ----------------------------------------------MCTS Validation-----------------------------------------------
     print("Run MCTS on the validation set... >>>", str_id, "<<< \n")
-    mcts = MCTS(exploration_weight=1.41, iterations=100)
+    env_test_post = gym.make(env_id, dict_input = env_kwargs_data['env_kwargs_test'], train_or_eval = "eval")
+
+    mcts = MCTS(exploration_weight=1.41, iterations=500)
 
     obs = env_test_post.reset()
     timesteps = Preprocess.eps_sim_steps_test
@@ -198,27 +203,28 @@ def main():
 
     for i in tqdm(range(timesteps), desc='---Apply MCTS planning on the test environment:'):
 
-        action = mcts.search(env_test_post)  # Perform MCTS search to get the best action
-        obs, _ , terminated, info = env_test_post.step(action)
+        action = mcts.search(env_test_post,i)  # Perform MCTS search to get the best action
+        obs, _ , _ , terminated, info = env_test_post.step(action)
+        print(info["Pot_Reward"], info["reward [ct]"], action)
 
         # Store data in stats
         if not terminated:
             j = 0
-            for val in info[0]:
+            for val in info:
                 if j < 24:
                     if val == 'Meth_Action':
-                        if info[0][val] == 'standby':
+                        if info[val] == 'standby':
                             stats[i, j] = 0
-                        elif info[0][val] == 'cooldown':
+                        elif info[val] == 'cooldown':
                             stats[i, j] = 1
-                        elif info[0][val] == 'startup':
+                        elif info[val] == 'startup':
                             stats[i, j] = 2
-                        elif info[0][val] == 'partial_load':
+                        elif info[val] == 'partial_load':
                             stats[i, j] = 3
                         else:
                             stats[i, j] = 4
                     else:
-                        stats[i, j] = info[0][val]
+                        stats[i, j] = info[val]
                 j += 1
         
     for m in range(len(EnvConfig.stats_names)):
@@ -233,6 +239,53 @@ def main():
     # PostProcess = Postprocessing(str_id, MCTSConfig, EnvConfig, TrainConfig, env_test_post, Preprocess)
     # PostProcess.test_performance()
     # PostProcess.plot_results()
+
+    plot_results(stats_dict_test, EnvConfig)
+
+def plot_results(stats_dict_test, EnvConfig):
+        """Generates a multi-subplot plot displaying time-series data and methanation operations based on the agent's actions"""
+        print("---Plot and save RL performance on the test set under ./plots/ ...\n") 
+
+        stats_dict = stats_dict_test
+        time_sim = stats_dict['steps_stats'] * EnvConfig.sim_step
+        time_sim *= 1 / 3600 / 24   # Converts the simulation time into days
+        time_sim = time_sim[:-6]    # ptg_gym_env.py curtails an episode by 6 time steps to ensure a data overhead
+        meth_state = stats_dict['Meth_State_stats'][:-6]+1
+
+        fig, axs = plt.subplots(3, 1, figsize=(10, 6), sharex=True, sharey=False)
+        axs[0].plot(time_sim, stats_dict['el_price_stats'][:-6], label='El.')
+        axs[0].plot(time_sim, stats_dict['gas_price_stats'][:-6], 'g', label='(S)NG')
+        axs[0].set_ylabel('El. & (S)NG prices\n [ct/kWh]')
+        axs[0].legend(loc="upper left", fontsize='small')
+        axs0_1 = axs[0].twinx()
+        axs0_1.plot(time_sim, stats_dict['eua_price_stats'][:-6], 'k', label='EUA')
+        axs0_1.set_ylabel('EUA prices [€/t$_{CO2}$]')
+        axs0_1.legend(loc="upper right", fontsize='small')
+
+        axs[1].plot(time_sim, meth_state, 'b', label='state')
+        axs[1].set_yticks([1,2,3,4,5])
+        axs[1].set_yticklabels(['Standby', 'Cooldown/Off', 'Startup', 'Partial Load', 'Full Load'])
+        axs[1].set_ylabel(' ')
+        axs[1].legend(loc="upper left", fontsize='small')
+        axs[1].grid(axis='y', linestyle='dashed')
+        axs1_1 = axs[1].twinx()
+        axs1_1.plot(time_sim, stats_dict['Meth_CH4_flow_stats'][:-6]*1000, 'yellowgreen', label='CH$_4$')
+        axs1_1.set_ylabel('CH$_4$ flow rate\n [mmol/s]')
+        axs1_1.legend(loc="upper right", fontsize='small')  
+        
+        axs[2].plot(time_sim, stats_dict['Meth_reward_stats'][:-6]/100, 'g', label='Reward')
+        axs[2].set_ylabel('Reward [€]')
+        axs[2].set_xlabel('Time [d]')
+        axs[2].legend(loc="upper left", fontsize='small')
+        axs2_1 = axs[2].twinx()
+        axs2_1.plot(time_sim, stats_dict['Meth_cum_reward_stats'][:-6]/100, 'k', label='Cum. Reward')
+        axs2_1.set_ylabel('Cumulative \n reward [€]')
+        axs2_1.legend(loc="upper right", fontsize='small')
+
+        fig.suptitle(f"{self.str_id} \n Rew: {np.round(stats_dict['Meth_cum_reward_stats'][-7]/100, 0)} €", fontsize=9)
+        plt.savefig(f'plots/{self.str_id}_plot.png')
+
+        plt.close()
 
 if __name__ == '__main__':
     main()
